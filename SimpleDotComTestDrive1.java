28ac2093a1237c9323aa47647c6e56389c0e64d4
TheKamsiYoung <47384524+TheKamsiYoung@users.noreply.github.com>
2019-02-06 16:11:54 +0100

    Added SimpleDotCom
    This is a game a similar to 'Sink the Battleships' but a really stripped down variant to a singular row.

---
+++ SimpleDotComTestDrive1.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class SimpleDotCom {
+	int[] locationCells;
+	int numOfHits;
+	
+	String checkUserValue(String value) {
+		int coordinate = Integer.parseInt(value);
+		String guess = "miss";
+		for (int count : locationCells) {
+			if (coordinate == count) {
+				numOfHits++;
+				if (numOfHits == 3) {
+					guess = "kill"; 
+				}
+				else {
+					guess = "hit";
+				}
+				break;
+			}
+		}
+		System.out.println(guess);
+		return guess;
+	}
+	void setLocationCells(int[] array) {
+		locationCells = array;
+	}
+}
+
+class GameHelper {
+	Scanner scan = new Scanner(System.in);
+	String userInput(String display) {
+		System.out.print(display);
+		String input = scan.nextLine();
+		return input;
+	}
+	void outputTotalGuess(int guess) {
+		System.out.println("Total number of guesses: " + guess);
+	}
+}
+
+public class SimpleDotComTestDrive1 {
+	public static void main (String[] args) {
+		int numOfGuesses = 0;
+		SimpleDotCom dot = new SimpleDotCom();
+		GameHelper helper = new GameHelper();
+		int randomNumber = (int) (Math.random()*5);
+		int[] location = {randomNumber, randomNumber + 1, randomNumber + 2};
+		dot.setLocationCells(location);
+		boolean isAlive = true;
+		while (isAlive == true) {
+			String userGuess = helper.userInput("Enter a number: ");
+			String result = dot.checkUserValue(userGuess);
+			numOfGuesses++;
+			if (result.equals("kill")) {
+				isAlive = false;
+				helper.outputTotalGuess(numOfGuesses);
+			}
+		}
+	}
+}
