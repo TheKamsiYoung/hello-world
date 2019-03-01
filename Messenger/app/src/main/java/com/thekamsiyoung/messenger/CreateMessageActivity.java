967238227ec74c2861d55fabd7b2b8f0b681f212
TheKamsiYoung <youngudochi15@gmail.com>
2019-03-01 11:28:59 +0100

    Added 'Messenger' exercise app

---
+++ Messenger/app/src/main/java/com/thekamsiyoung/messenger/CreateMessageActivity.java
@@ -0,0 +1,14 @@
+package com.thekamsiyoung.messenger;
+
+import android.app.*;
+import android.os.*;
+
+public class CreateMessageActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
